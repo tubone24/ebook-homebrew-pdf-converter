9994416ece69613a7807f289bce7677fe6bb7b73
Yu Otsubo <uu-ootsubo@kddi.com>
2019-08-14 20:25:07 +0900

    change echo

--- main.go
+++ main.go
@@ -1,15 +1,53 @@
 package main
 
 import (
-	_ "ebook-homebrew-pdf-converter/routers"
+	"net/http"
+	"strconv"
 
-	"github.com/astaxie/beego"
+	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
 )
 
+// User is user strcut
+type User struct {
+	ID      int    `json:"id"`
+	GroupID int    `json:"group_id"`
+	Name    string `json:"name"`
+	Gender  string `json:"gender"`
+}
+
 func main() {
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	e := echo.New()
+
+	initRouting(e)
+
+	e.Logger.Fatal(e.Start(":1313"))
+}
+
+func initRouting(e *echo.Echo) {
+	e.GET("/", status)
+	e.GET("/api/v1/groups/:group_id/users", getUsers)
+}
+
+func status(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]string{"status": "ok"})
+}
+
+func getUsers(c echo.Context) error {
+	groupIDStr := c.Param("group_id")
+	groupID, err := strconv.Atoi(groupIDStr)
+	if err != nil {
+		return errors.Wrapf(err, "errors when group id convert to int: %s", groupIDStr)
 	}
-	beego.Run()
+	gender := c.QueryParam("gender")
+	users := []*User{}
+	if gender == "" || gender == "man" {
+		users = append(users, &User{ID: 1, GroupID: groupID, Name: "Taro", Gender: "man"})
+		users = append(users, &User{ID: 2, GroupID: groupID, Name: "Jiro", Gender: "man"})
+	}
+	if gender == "" || gender == "woman" {
+		users = append(users, &User{ID: 3, GroupID: groupID, Name: "Hanako", Gender: "woman"})
+		users = append(users, &User{ID: 4, GroupID: groupID, Name: "Yoshiko", Gender: "woman"})
+	}
+	return c.JSON(http.StatusOK, users)
 }
